5798a2a29cd14b25a42193a46caee80aabcaa271
icebear2n2 <icebear2n2@gmail.com>
2023-01-24 17:44:59 +0900

    time error

--- step_3/1110.py
+++ step_3/1110.py
@@ -1,19 +1,30 @@
-n = str(input())
+import sys
+
+n = str(sys.stdin.readline())
 lst = [0, 0]
-result = n
 count = 0
 
 if int(n) <= 10:
     n = '0{}'.format(n)
 
+result = n
+
 while True:
     count += 1
     lst[0] = n[1]
     lst[1] = str((int(n[0]) + int(n[1])) % 10)
-
     n = ''.join(lst)
 
     if result == n:
         break
 
 print(count)
+
+
+
+
+
+
+
+
+
