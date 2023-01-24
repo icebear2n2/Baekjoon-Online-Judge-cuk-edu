8d4291933f30522ddb0b036babbef74352d33eb7
icebear2n2 <icebear2n2@gmail.com>
2023-01-24 17:05:41 +0900

    ing .. 80%

--- step_3/1110.py
+++ step_3/1110.py
@@ -1,1 +1,19 @@
-n = int(input())
+n = str(input())
+lst = [0, 0]
+result = n
+count = 0
+
+if int(n) <= 10:
+    n = '0{}'.format(n)
+
+while True:
+    count += 1
+    lst[0] = n[1]
+    lst[1] = str((int(n[0]) + int(n[1])) % 10)
+
+    n = ''.join(lst)
+
+    if result == n:
+        break
+
+print(count)
