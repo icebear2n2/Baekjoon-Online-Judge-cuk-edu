b9e4fef8594df861475f8d6bf240549345a5ebc4
icebear2n2 <icebear2n2@gmail.com>
2023-01-30 13:29:20 +0900

    1110

--- step_3/1110.py
+++ step_3/1110.py
@@ -1,30 +1,37 @@
-import sys
+# n >= 10인 경우의 풀이
 
-n = str(sys.stdin.readline())
-lst = [0, 0]
-count = 0
+# n = str(input()) 
+# num = n
+# lst = list(n)
+# count = 0
 
-if int(n) <= 10:
+# while True:
+#     count += 1
+#     lst[0] = n[1]
+#     lst[1] = str((int(n[0]) + int(n[1])) % 10)  
+
+#     n = ''.join(lst) 
+#     if n == num:
+#         break
+
+# print(count)
+
+n = str(input()) 
+
+if len(n) < 2:
     n = '0{}'.format(n)
 
-result = n
+num = n
+lst = list(n)
+count = 0
 
 while True:
     count += 1
     lst[0] = n[1]
-    lst[1] = str((int(n[0]) + int(n[1])) % 10)
-    n = ''.join(lst)
+    lst[1] = str((int(n[0]) + int(n[1])) % 10)  
 
-    if result == n:
+    n = ''.join(lst) 
+    if n == num:
         break
 
 print(count)
-
-
-
-
-
-
-
-
-
