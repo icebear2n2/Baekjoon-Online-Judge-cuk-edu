b2b3ccf86c8a1149a8b5a203dae3c71f39f00ab3
icebear2n2 <icebear2n2@gmail.com>
2023-01-30 13:29:59 +0900

    1110

--- step_3/1110.py
+++ step_3/1110.py
@@ -1,21 +1,3 @@
-# n >= 10인 경우의 풀이
-
-# n = str(input()) 
-# num = n
-# lst = list(n)
-# count = 0
-
-# while True:
-#     count += 1
-#     lst[0] = n[1]
-#     lst[1] = str((int(n[0]) + int(n[1])) % 10)  
-
-#     n = ''.join(lst) 
-#     if n == num:
-#         break
-
-# print(count)
-
 n = str(input()) 
 
 if len(n) < 2:
