f72adb5873563789c4a63f9bbb58dfd17e054bbc
icebear2n2 <icebear2n2@gmail.com>
2023-02-25 20:08:26 +0900

    ing..

--- step_6/1157.py
+++ step_6/1157.py
@@ -1,4 +1,15 @@
-n = str(input())
+n = list(str(input()).upper())
+count = []
 
-for i in n:
-    
+for i in set(n):
+    if n.count(i) > 1:
+        count.append(i)
+
+if len(n) == 1:
+    print(n[0])
+
+elif len(n) == len(set(n)) or len(count) > 1:
+    print('?')
+
+else:
+    print(count[0])
