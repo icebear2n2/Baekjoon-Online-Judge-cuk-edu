41617ba8e5081b29aefa252bceabab03fc417e6c
icebear2n2 <icebear2n2@gmail.com>
2023-02-26 16:48:01 +0900

    Yess~~!

--- step_6/1157.py
+++ step_6/1157.py
@@ -1,8 +1,28 @@
 n = list(str(input()).upper())
 dic = {}
+count = 0
+result = ''
 
 for i in set(n):
     dic[i] = n.count(i)
 
-sort_dict = dict(sorted(dic.items(), key=lambda x: x[1]))
+sort_dict = dict(sorted(dic.items(), key=lambda x: x[1], reverse=True))
 
+if len(n) == 1:
+    print(n[0])
+
+for key in sort_dict.keys():
+    count += 1
+
+    if count == 1:
+        result = key
+
+    elif count > 1:
+
+        if sort_dict[result] == sort_dict[key]:
+            print('?')
+            break
+
+        else:
+            print(result)
+            break
