e11f77ea44d86c0fb570cd35346cb7443ae99f4c
icebear2n2 <icebear2n2@gmail.com>
2023-02-26 17:16:47 +0900

    ing ..

--- step_4/10813.py
+++ step_4/10813.py
@@ -1,3 +1,25 @@
 n, m = map(int, input().split())
-lst = []
+dic = {}
+result = ''
+save = 0
+
+for i in range(1, n+1):
+    dic[i] = i
+
+for i in range(m):
+    balls = list(map(int, input().split()))
+
+    if balls[0] in dic:
+        save = dic[balls[0]]
+        dic[balls[0]] = dic[balls[1]]
+        dic[balls[1]] = save
+    elif balls[1] in dic:
+        save = dic[balls[1]]
+        dic[balls[1]] = dic[balls[0]]
+        dic[balls[0]] = save
+
+for val in dic.values():
+    result += str(val)
+
+print(' '.join(result))
 
