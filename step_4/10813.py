c3ffb0a10c567d0bb5a483566c39d2d680c946e6
icebear2n2 <icebear2n2@gmail.com>
2023-02-28 14:40:08 +0900

    10813

--- step_4/10813.py
+++ step_4/10813.py
@@ -1,25 +1,16 @@
 n, m = map(int, input().split())
 dic = {}
-result = ''
 save = 0
-
+save2 = 0
 for i in range(1, n+1):
     dic[i] = i
 
 for i in range(m):
     balls = list(map(int, input().split()))
+    save = dic[balls[0]]
+    save2 = dic[balls[1]]
 
-    if balls[0] in dic:
-        save = dic[balls[0]]
-        dic[balls[0]] = dic[balls[1]]
-        dic[balls[1]] = save
-    elif balls[1] in dic:
-        save = dic[balls[1]]
-        dic[balls[1]] = dic[balls[0]]
-        dic[balls[0]] = save
+    dic[balls[0]] = save2
+    dic[balls[1]] = save
 
-for val in dic.values():
-    result += str(val)
-
-print(' '.join(result))
-
+print(*dic.values())
