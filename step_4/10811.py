d58f6eec141b0c2d634ff068918132c1448668fd
icebear2n2 <icebear2n2@gmail.com>
2023-03-03 15:28:03 +0900

    ing ..

--- step_4/10811.py
+++ step_4/10811.py
@@ -1,19 +1,2 @@
 n, m = map(int, input().split())
 dic = {}
-save = 0
-
-for i in range(1, n+1):
-    dic[i] = i
-
-for i in range(m):
-    ball_1, ball_2 = map(int, input().split())
-    balls = [ i for i in range(ball_1, ball_2+1)]
-
-    if len(balls) <= 2:
-        save = dic[ball_1]
-        dic[ball_1] = dic[ball_2]
-        dic[ball_2] = save
-
-        print(dic)
-    else:
-        pass
